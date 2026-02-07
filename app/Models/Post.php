3cbb9e95ab15ee84c50737a16428647f9c90d524
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2026-02-07 18:54:49 +0000

    Créer le modèle Post et la migration associée

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    /**
+     * Get the user that owns the post.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
