cf9a054413cc7b3a1416c120dde1034d740117b1
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2026-02-07 12:33:33 +0000

    Créer le modèle User et la migration associée

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-user', function () {
+    $user = new User();
+
+    $user->first_name = 'John';
+    $user->last_name = 'Doe';
+    $user->username = 'johndoe';
+    $user->email = 'johndoe@example.com';
+    $user->password = 'password';
+
+    $user->save();
+
+    return $user;
 });
