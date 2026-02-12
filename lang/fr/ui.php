8921ad7518332787ab6170fd3a4f9690f5874f69
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2026-02-12 15:09:18 +0000

    Mettre en place l'internationalisation (i18n)

---
+++ lang/fr/ui.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'home' => [
+        'title' => 'Accueil',
+        'description' => "Page d'accueil du réseau social.",
+        'welcome_message' => 'Bienvenue sur :app_name !',
+    ],
+    'profile' => [
+        'title' => 'Profil',
+        'description' => 'Page de profil pour :username.',
+        'number_of_posts' => '{0} Aucune publication|{1} :count publication|[2,*] :count publications',
+    ],
+    'about' => [
+        'title' => 'À propos',
+        'description' => 'Page à propos de notre réseau social.',
+        'introduction' => 'Ce réseau social a été créé pour permettre aux utilisateur.trices de partager leurs pensées et leurs idées avec le monde entier.',
+        'disclaimer' => "Ce réseau social est un projet réalisé dans le cadre d'un cours de la HEIG-VD, Suisse.",
+    ],
+];
